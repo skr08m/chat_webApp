0c389c407b578927861bb755b8199f2ca702572e
skr08m <sakuraba.mizuki08@gmail.com>
2025-07-05 18:56:00 +0900

    初回コミット

---
+++ backend/src/test/java/com/example/chat_webapp/ChatWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
